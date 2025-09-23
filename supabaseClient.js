405a72069ac7f81325b4cc4d5a0fd921e8450d68
reapersanssss <118244867+reapersanssss@users.noreply.github.com>
2025-09-22 22:54:17 -0300

    sapabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://mcfsycndtlkpvburzxfy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jZnN5Y25kdGxrcHZidXJ6eGZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxNDE2ODMsImV4cCI6MjA3MzcxNzY4M30.R1qJfLmJ0Dt7UoF59vY-5FQWCDHK5gfy1M_kTYLkobQ";
+const SUPABASE_URL = "https://jwmbpltpmpkqczxdoave.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bWJwbHRwbXBrcWN6eGRvYXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1OTEwNDcsImV4cCI6MjA3NDE2NzA0N30.fOIG41NcI5lpop34eg-TsymepReu2PHysu_ESkU5m_Q";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
