c5f0c0f67d3be9d44f1e327acdd96204ef0a8a00
reapersanssss <118244867+reapersanssss@users.noreply.github.com>
2025-10-11 21:31:51 -0300

    cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const SUPABASE_URL = "https://jwmbpltpmpkqczxdoave.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_CHAVE_ANONIMA_AQUI";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bWJwbHRwbXBrcWN6eGRvYXZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1OTEwNDcsImV4cCI6MjA3NDE2NzA0N30.fOIG41NcI5lpop34eg-TsymepReu2PHysu_ESkU5m_Q";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
